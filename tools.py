948607e1400def4b53738bdcd04959e8d2850732
dsaucez <damien.saucez@gmail.com>
2022-05-24 10:09:31 +0000

    Descriptif

---
+++ tools.py
@@ -0,0 +1,22 @@
+# Import libraries
+import matplotlib.pyplot as plt
+import csv
+from math import log
+from math import exp
+
+def loadData(filename):
+    """
+    Charge les données d'un fichier CSV
+    """
+    with open(filename, newline='', encoding='UTF-8') as csvfile:
+        tons = {}
+        data = csv.reader(csvfile, delimiter=',', quotechar='#')
+        header = next(data)
+        sources = next(data)
+        for row in data:
+            tons[int(row[0])] = int(row[1])
+
+        X = list(tons.keys())
+        Y = list(tons.values())
+
+        return X, Y
